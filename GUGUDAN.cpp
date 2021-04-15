7be6b91954bb5ee3325af30ff67db5c67a66730c
Dohyun <chopu66@naver.com>
2021-04-15 20:02:11 +0900

    10단까지 수정

--- GUGUDAN.cpp
+++ GUGUDAN.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main()
 {
 	cout << "gugudan 1~8" << endl;
-	for (int i = 1; i < 9; i++)
+	for (int i = 1; i < 10; i++)
 	{
-		for (int j = 1; j < 9; j++)
+		for (int j = 1; j < 10; j++)
 			cout << i << "x" << j << " = " << i * j << " ";
 		cout << endl;
 	}
